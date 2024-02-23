b40dc27ab7106710fc2cc9ed4675c72ed5b3f367
Rahman <rahmansaif.413@gmail.com>
2024-02-23 15:54:55 +0600

    added a class containing functions!

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
 
-
-
-
-
-function randomHexColorCode(){
-    let n=(Math.random()*0xfffff*1000000).toString(16);
-    return '#'+n.slice(0,6);
-}
-
 let text=document.createElement("p");
 let nav=document.querySelector(".nav");
 let bgColorText=document.querySelector(".bg-color");
 let button=document.querySelector(".click_me");
 let main=document.querySelector("#main")
 
-// button && button.addEventListener('click',(){
-//     console.log("button");
-// })
 
-
-
-
-button.addEventListener("click",function(){
-    let colorCode=randomHexColorCode();
-    text.innerText=colorCode;
-    bgColorText.appendChild(text);
-
-    main.style.backgroundColor=colorCode;
-});
+button.addEventListener("click",backgroundColorChanger);
 
 
     
